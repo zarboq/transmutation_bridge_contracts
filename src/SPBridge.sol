3faaa7a136b9967c7ed8fcec92da838e7aef8550
TurcFort07 <boramindas@gmail.com>
2023-07-08 18:33:43 +0200

    fix mapping user

--- src/SPBridge.sol
+++ src/SPBridge.sol
@@ -11,16 +11,16 @@ contract SPBridge {
     }
 
     // counter used on L2 to check if funds have been unstaked between two operations
-    mapping(address => UserInfo) public tokenToUser;
+    mapping(address => mapping(address => UserInfo)) public tokenToUser;
 
     function lock(address _token, uint256 _amount) external {
         IERC20(_token).transferFrom(msg.sender, address(this), _amount);
-        UserInfo storage user = tokenToUser[_token];
+        UserInfo storage user = tokenToUser[_token][msg.sender];
         user.amount += _amount;
     }
 
     function unlock(address _token, uint256 _amount) external {
-        UserInfo storage user = tokenToUser[_token];
+        UserInfo storage user = tokenToUser[_token][msg.sender];
         user.counter += 1;
         user.amount -= _amount;
         IERC20(_token).transfer(msg.sender, _amount);
