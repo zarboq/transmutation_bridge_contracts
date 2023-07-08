fbfc7434ff0931b125351d414ff35290910b3e31
TurcFort07 <boramindas@gmail.com>
2023-07-08 18:10:58 +0200

    feat: SPBridge contract

--- src/SPBridge.sol
+++ src/SPBridge.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/interfaces/IERC20";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SPBridge {
 
@@ -14,14 +14,15 @@ contract SPBridge {
     mapping(address => UserInfo) public tokenToUser;
 
     function lock(address _token, uint256 _amount) external {
-        
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        UserInfo storage user = tokenToUser[_token];
+        user.amount += _amount;
     }
 
     function unlock(address _token, uint256 _amount) external {
-
-        
         UserInfo storage user = tokenToUser[_token];
         user.counter += 1;
         user.amount -= _amount;
+        IERC20(_token).transferFrom(address(this), msg.sender, _amount);
     }
 }
